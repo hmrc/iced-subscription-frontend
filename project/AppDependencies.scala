8722ddfa922edeef06ad7aefd7cc617a1be05390
marjan <marjan.mubarok@digital.hmrc.gov.uk>
2021-03-30 14:45:43 +0100

    DL-5086 update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,15 +1,11 @@
-import play.core.PlayVersion.current
-import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "3.4.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "0.38.0-play-27",
-    "uk.gov.hmrc" %% "play-frontend-govuk"        % "0.60.0-play-27",
-    "uk.gov.hmrc" %% "play-language"              % "4.10.0-play-27"
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "0.57.0-play-27",
+    "uk.gov.hmrc" %% "play-language"              % "4.12.0-play-27"
   )
 
   val test = Seq(
