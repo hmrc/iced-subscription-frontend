bb341b623e2990c490df8afcaec7b9fd9f4ca103
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2021-05-06 12:10:42 +0100

    DL-5408 dependency and logback updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,13 +4,13 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "3.4.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "0.57.0-play-27",
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "0.60.0-play-27",
     "uk.gov.hmrc" %% "play-language"              % "4.12.0-play-27"
   )
 
   val test = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-27" % "3.4.0",
-    "org.scalatest"          %% "scalatest"              % "3.2.7",
+    "org.scalatest"          %% "scalatest"              % "3.2.8",
     "org.scalamock"          %% "scalamock"              % "5.1.0",
     "org.jsoup"              % "jsoup"                   % "1.13.1",
     "com.typesafe.play"      %% "play-test"              % "2.7.9",
