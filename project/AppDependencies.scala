cdbbff8b9855e4ed34f7dc3fbfb4e00a23058b98
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2023-01-09 15:47:12 +0000

    DL-9355 update play frontend hmrc and other dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,15 +5,14 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.25.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "3.21.0-play-28",
-    "uk.gov.hmrc" %% "play-language"              % "5.3.0-play-28"
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "4.1.0-play-28",
+    "uk.gov.hmrc" %% "play-language"              % "6.1.0-play-28"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % "5.25.0",
-    "org.scalamock"          %% "scalamock"              % "5.2.0",
-    "org.jsoup"              % "jsoup"                   % "1.15.1",
-    "com.typesafe.play"      %% "play-test"              % PlayVersion.current,
-    "org.pegdown"            % "pegdown"                 % "1.6.0"
+    "uk.gov.hmrc"       %% "bootstrap-test-play-28" % "5.25.0",
+    "org.scalamock"     %% "scalamock"              % "5.2.0",
+    "org.jsoup"         % "jsoup"                   % "1.15.3",
+    "com.typesafe.play" %% "play-test"              % PlayVersion.current
   ).map(_ % "test, it")
 }
