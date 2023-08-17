5bdeedb65e51cf47392b30460fdaefb028ce6b38
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2023-08-17 16:57:23 +0100

    DL-10741 dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,14 +4,14 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.25.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "6.2.0-play-28"
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.21.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "7.19.0-play-28"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-test-play-28" % "5.25.0",
+    "uk.gov.hmrc"       %% "bootstrap-test-play-28" % "7.21.0",
     "org.scalamock"     %% "scalamock"              % "5.2.0",
-    "org.jsoup"         % "jsoup"                   % "1.15.3",
+    "org.jsoup"         % "jsoup"                   % "1.16.1",
     "com.typesafe.play" %% "play-test"              % PlayVersion.current
   ).map(_ % "test, it")
 }
