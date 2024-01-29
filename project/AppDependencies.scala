80ded53df85a0322fbd6ea2ce259ec7bad93c803
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2024-01-29 12:03:59 +0000

    DL-12283 minor tweak and update dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,12 @@ import sbt._
 
 object AppDependencies {
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.22.0",
-    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-28"            % "8.3.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-28" % "8.4.0",
   )
 
-  val test = Seq(
+  val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-28" % "7.22.0",
     "org.scalamock"     %% "scalamock"              % "5.2.0",
     "org.jsoup"         %  "jsoup"                  % "1.16.1",
