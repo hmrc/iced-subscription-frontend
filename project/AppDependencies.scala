6b743ec9e8107f11d5741936f01d0b98c97886a8
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2024-06-24 16:40:23 +0100

    DL-13839 play frontend upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,17 +2,17 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "8.4.0"
+  val bootstrapVersion = "8.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "8.5.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "10.2.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30" % bootstrapVersion,
     "org.scalamock"     %% "scalamock"              % "5.2.0",
-    "org.jsoup"         %  "jsoup"                  % "1.16.1"
+    "org.jsoup"         %  "jsoup"                  % "1.17.2"
   ).map(_ % Test)
 
 }
