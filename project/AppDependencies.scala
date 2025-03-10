ab0a97439b965743e3ceb70daed1203e7de90b0a
christianmennerich <177663202+christianmennerich@users.noreply.github.com>
2025-03-10 12:13:38 +0000

    DL-15631 - Updates dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,17 +2,17 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.1.0"
+  val bootstrapVersion = "9.11.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "10.5.0"
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "11.13.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30" % bootstrapVersion,
-    "org.scalamock"     %% "scalamock"              % "6.0.0",
-    "org.jsoup"         %  "jsoup"                  % "1.18.1"
+    "org.scalamock"     %% "scalamock"              % "6.2.0",
+    "org.jsoup"         %  "jsoup"                  % "1.19.1"
   ).map(_ % Test)
 
 }
